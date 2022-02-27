07d02c485a6b8c5fc0b6bfb0510e07c30c9818e9
Vesna1992 <veca1710@gmail.com>
2022-02-27 17:46:45 +0100

    Update footer.php

--- shared/footer.php
+++ shared/footer.php
@@ -4,7 +4,8 @@
         <div>
             <article>
                 <h3><i class="fas fa-camera"></i>&nbsp;Photo studio</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nisi eaque aut, soluta et blanditiis?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nisi eaque aut, soluta et
+                    blanditiis?</p>
                 <a href=""><i class="fab fa-facebook-square"></i></a>
                 <a href=""><i class="fab fa-instagram"></i></a>
                 <a href=""><i class="fab fa-twitter"></i></a>
@@ -19,12 +20,12 @@
             <article>
                 <h3>Quick links</h3>
                 <div class="links">
-                    <a href="">Home</a>
-                    <a href="">Gallery</a>
-                    <a href="">About</a>
-                    <a href="">Portfolio</a>
-                    <a href="">Contact</a>
-                    <a href="">Services</a>
+                    <a href="index.php">Home</a>
+                    <a href="index.php#gallery">Gallery</a>
+                    <a href="about.php">About</a>
+                    <a href="index.php#gallery">Portfolio</a>
+                    <a href="contact.php">Contact</a>
+                    <a href="pricing.php">Services</a>
                 </div>
             </article>
             <article>
@@ -40,6 +41,7 @@
             <small>&copy; 2021. by Vesna1992</small>
         </div>
     </article>
-</footer>  
+</footer>
 </body>
+
 </html>
