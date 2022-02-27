88720b629259d3547f1d57f10faaa71aa98e225c
Vesna1992 <veca1710@gmail.com>
2022-02-27 17:51:32 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 
+// show/hide nav
+const toggler = document.querySelector('.toggler')
+const bars = document.querySelectorAll('.bar')
+const nav = document.querySelector('nav')
+const ul = document.querySelector('nav ul')
+const loadMoreBtn = document.querySelector('.load-more-btn');
+const gallery2 = document.querySelector('.gallery2');
+
+const toggleNavDisplay = () => {
+    ul.classList.toggle('active-ul')
+    bars.forEach(bar => bar.classList.toggle('active'))
+}
+
+toggler.addEventListener('click', toggleNavDisplay)
+
+// toggle gallery2
+if(loadMoreBtn) {
+    loadMoreBtn.addEventListener('click', function(e) {
+        gallery2.classList.toggle('show');
+        e.target.innerHTML == 'More...'? e.target.innerHTML = 'Less' : e.target.innerHTML = 'More...'
+    })
+}
